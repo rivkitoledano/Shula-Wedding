80d45504b94adc9181797028c8ca408fe2edd813
yulifrank <yaelfrank0338@gmail.com>
2024-06-07 16:05:15 +0300

    add animation circle

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import React from "react";
 import CountDown from "./countDown/CountDown";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <CountDown />
-   
+      <div className="circle" id="circle1"></div>
+      <div className="circle" id="circle2"></div>
+      <div className="circle" id="circle3"></div>
+      <div className="circle" id="circle4"></div>
+      <div className="circle" id="circle5"></div>
+      <div className="circle" id="circle6"></div>
+      <div className="circle" id="circle7"></div>
+      <div className="circle" id="circle8"></div>
     </div>
   );
 }
 
 export default App;
-
